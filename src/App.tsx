d6b27620f8ae9ed3cb0d10ba8d44b002340cc8ce
Ginieee <kaj1226@naver.com>
2024-04-18 20:53:21 +0900

    ✨ [Feat] 경로별 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+import UserListPage from "./pages/UserListPage";
+import ReportReceptionPage from "./pages/ReportReceptionPage";
+import ReportProcessPage from "./pages/ReportProcessPage";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
+      <RecoilRoot>
+        <Routes>
+          <Route path="/" element={<MainPage />}>
+            <Route index element={<Navigate replace to="/user/list" />} />
+            <Route path="user/list" element={<UserListPage />} />
+            <Route path="report/reception" element={<ReportReceptionPage />} />
+            <Route path="report/process" element={<ReportProcessPage />} />
+          </Route>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </RecoilRoot>
     </Router>
   );
 }
